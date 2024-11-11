010994959eb8df67f6257d9262e2033189f5909a
Curtis Smith <CLSmith21@student.fullsail.edu>
2024-11-11 11:45:01 -0800

    throwobject and projectileaddon

---
+++ Team_Dragon_Zombies/Assets/Scripts/ProjectileAddon.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class StickyProjectileAddon : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private bool targetHit;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     rb = GetComponent<Rigidbody>();   
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // make sure only to stick to the first target you hit
+        if (targetHit)
+            return;
+        else
+            targetHit = true;
+
+        // make sure projectile sticks to surface
+        rb.isKinematic = true;
+
+        // make sure projectile moves with target
+        transform.SetParent(collision.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Team_Dragon_Zombies/Assets/Scripts/ThrowObjects.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class ThrowObjects : MonoBehaviour
+{
+    [Header("Reference")]
+    public Transform cam;
+    public Transform attackPoint;
+    public GameObject objectToThrow;
+
+    [Header("Settings")]
+    public int totalThrows;
+    public float throwCooldown;
+
+    [Header("Throwing")]
+    public KeyCode throwKey = KeyCode.Mouse0;
+    public float throwForce;
+    public float throwUpwardForce;
+
+    bool readyToThrow;
+
+    private void Start()
+    {
+        readyToThrow = true;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(throwKey) && readyToThrow && totalThrows > 0)
+        {
+            Throw();
+        }
+    }
+
+    private void Throw()
+    {
+        readyToThrow = false;
+
+        // instantiate object to throw 
+        GameObject projectile = Instantiate(objectToThrow, attackPoint.position, cam.rotation); 
+
+        // get rigibody component
+        Rigidbody projectileRB = projectile.GetComponent<Rigidbody>();
+
+        // calculate direction
+        Vector3 forceDirection = cam.transform.forward;
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(cam.position, cam.forward, out hit, 500f))
+        {
+            forceDirection = (hit.point - attackPoint.position).normalized;
+        }
+
+        // add force
+        Vector3 forceToAdd = forceDirection * throwForce + transform.up * throwUpwardForce;
+
+        projectileRB.AddForce(forceToAdd, ForceMode.Impulse);
+
+        // implement throwCooldown
+        Invoke(nameof(ResetThrow), throwCooldown);
+    }
+
+    private void ResetThrow()
+    {
+        readyToThrow = true;
+    }
+
+}
