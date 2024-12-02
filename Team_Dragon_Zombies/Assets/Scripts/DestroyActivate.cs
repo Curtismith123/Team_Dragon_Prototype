27298138f39896bfaa511b38abd3f01eafcd9f29
Rburnrebel <REBernat@student.fullsail.edu>
2024-12-02 13:03:50 -0800

    Added Showcase Area
    Added Showcase Area with:
    
    Enemy Display
    Weapons
    Thrown and Explode

--- Team_Dragon_Zombies/Assets/Scripts/DestroyActivate.cs
+++ Team_Dragon_Zombies/Assets/Scripts/DestroyActivate.cs
@@ -6,6 +6,18 @@ public class DestroyActivate : MonoBehaviour
     public GameObject objectToActivate;
     public float delayTime = 2f;
 
+    private void Start()
+    {
+        if (objectToDestroy != null)
+        {
+            DestroyHelper helper = objectToDestroy.GetComponent<DestroyHelper>();
+            if (helper != null)
+            {
+                helper.RegisterOnDestroyed(OnObjectDestroyed);
+            }
+        }
+    }
+
     public void OnObjectDestroyed()
     {
         Invoke("ActivateObject", delayTime);
