c67d9d48e9388250073db2cb0f8bcdf34320ba51
CeliBananaz <159220754+CeliBananaz@users.noreply.github.com>
2024-12-03 14:25:13 -0700

    Fixed loading screen by adding a simulated pause
    Added a 3 second simulated pause, along with a simulated loading bar + text

--- Team_Dragon_Zombies/Assets/Scripts/SceneLoader.cs
+++ Team_Dragon_Zombies/Assets/Scripts/SceneLoader.cs
@@ -21,6 +21,8 @@ public class SceneLoader : MonoBehaviour
         }
     }
 
+
+
     private IEnumerator LoadLevel()
     {
         if (loadingScreen != null)
@@ -31,20 +33,33 @@ public class SceneLoader : MonoBehaviour
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
         operation.allowSceneActivation = false;
 
+        //simulating progress control
+        progressBar.fillAmount = 0.5f;
+        progressText.text = "50%";
+        yield return new WaitForSeconds(1f);
+
+        progressBar.fillAmount = 0.85f;
+        progressText.text = "85%";
+        yield return new WaitForSeconds(1f);
+
         while (!operation.isDone)
         {
             float progress = Mathf.Clamp01(operation.progress / 0.9f);
-            progressBar.fillAmount = progress;
-            progressText.text = Mathf.RoundToInt(progress * 100) + "%";
+
+            //progressBar.fillAmount = progress;
+            //progressText.text = Mathf.RoundToInt(progress * 100) + "%";
 
             if (operation.progress >= 0.9f)
             {
+                //final addition to the simulation control
+                progressBar.fillAmount = 1f;
+                progressText.text = "100%";
+                yield return new WaitForSeconds(1f);
                 operation.allowSceneActivation = true;
             }
 
             yield return null;
 
         }
-
     }
 }
