34afcbc7d35259c9055d61d0345d6120e10fc641
espacheco <166456561+espacheco@users.noreply.github.com>
2024-12-08 12:48:50 -0700

    Added a controls menu

--- Team_Dragon_Zombies/Assets/Scripts/RebindingControls.cs
+++ Team_Dragon_Zombies/Assets/Scripts/RebindingControls.cs
@@ -4,24 +4,27 @@ using UnityEngine.InputSystem;
 
 public class RebindingContols : MonoBehaviour
 {
-    public InputActionReference m_Action; // Reference to an action to rebind.
-    public int m_BindingIndex; // Index into m_Action.bindings for binding to rebind.
-    public TMP_Text m_DisplayText; // Text in UI that receives the binding display string.
+    //public InputActionReference m_Action; // Reference to an action to rebind.
+    //public int m_BindingIndex; // Index into m_Action.bindings for binding to rebind.
+    //public TMP_Text m_DisplayText; // Text in UI that receives the binding display string.
 
-    [SerializeField] private InputActionReference inputAction;
-    //[SerializeField] private PlayerController playerController;
-    //[SerializeField] private TMP_Text bindDisplayTxt;
+    //[SerializeField] private InputActionReference inputAction;
+    [SerializeField] private TMP_Text actionLabel;
+    [SerializeField] private TMP_Text bindDisplayTxt;
     [SerializeField] private GameObject rebindStart;
     [SerializeField] private GameObject waitingInput;
 
     public void startRebinding()
     {
+        bindDisplayTxt.text = "";
         rebindStart.SetActive(false);
         waitingInput.SetActive(true);
 
-        string keyRebind = Input.GetButtonDown("Vertical").ToString();
-        
-        
+        string keyRebind = Input.GetButtonUp(name).ToString();
+        bindDisplayTxt.text = keyRebind;
+
+        waitingInput.SetActive(false);
+        rebindStart.SetActive(true);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
