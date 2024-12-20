0fdbddf711ba159d1046c14f488d6e178d92b207
CeliBananaz <159220754+CeliBananaz@users.noreply.github.com>
2024-12-19 18:20:19 -0700

    Fixed pause menu and tutorial text overlap

--- Team_Dragon_Zombies/Assets/Scripts/UITextCollider.cs
+++ Team_Dragon_Zombies/Assets/Scripts/UITextCollider.cs
@@ -5,15 +5,26 @@ public class UITextCollider : MonoBehaviour
 {
     [SerializeField] GameObject uiObject;
     public int secToWait;
+
+    private bool hasTriggered = false;
     void Start()
     {
         uiObject.SetActive(false);
     }
 
+    private void Update()
+    {
+        if (hasTriggered)
+        {
+            uiObject.SetActive(!gameManager.instance.isPaused);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && !gameManager.instance.isPaused)
         {
+            hasTriggered = true;
             uiObject.SetActive(true);
             StartCoroutine("WaitForSec");
         }
@@ -22,8 +33,11 @@ public class UITextCollider : MonoBehaviour
     IEnumerator WaitForSec()
     {
         yield return new WaitForSeconds(secToWait);
-        Destroy(uiObject);
-        Destroy(gameObject);
+        if (!gameManager.instance.isPaused)
+        {
+            Destroy(uiObject);
+            Destroy(gameObject);
+        }
 
     }
 
